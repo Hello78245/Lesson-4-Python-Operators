91154fe7429b882285d8b8bdc2872bc3f9a477ff
Hello78245 <shinimagesh10@gmail.com>
2026-01-12 19:02:44 -0800

    OperationWIthFriendlySquares

---
+++ OperationWithFriendlySquares.py
@@ -0,0 +1,31 @@
+#storing values
+
+Square1=48
+Square2=4
+Square3=50
+Square4=23
+Square5=43
+Square6=11
+Square7=30
+Square8=39
+Square9=10
+Square10=26
+Square11=28
+Square12=29
+Square13=21
+Square14=699
+Square15=154
+Square16=513
+Square17=514
+Square18=989
+Square19=1125
+Square20=1000
+Square21=178
+#finding the total of the squares
+sum= Square1+Square2+Square3+Square4+Square5+Square6+Square7+Square8+Square9+Square10+Square11+Square12+Square13+Square14+Square15+Square16+Square17+Square18+Square19+Square20+Square21
+print("The sum of all of the friendly squares are:",sum)
+
+#Finding the average of the friendly squares
+
+average= sum/21
+print("The average of all the friendly squares are:",average)
